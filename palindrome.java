87f7b9ea48a870f7595157a73e4f03b473c19b10
Christian Vincent Villanueva <villanuevachristianvincent@gmail.com>
2023-06-14 16:15:25 +0800

    first commit

---
+++ palindrome.java
@@ -0,0 +1,20 @@
+public class palindrome {
+    public static void main(String[] args) {
+        String original = "racecar";
+        String reversed = "";
+
+        // Reverse the original string
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        // Check if the original and reversed strings are equal
+        boolean isPalindrome = original.equals(reversed);
+
+        if (isPalindrome) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+    }
+}
---
+++ pattern.java
@@ -0,0 +1,19 @@
+public class pattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            
+            // Print asterisks
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println(); // Move to the next line
+        }
+    }
+}
